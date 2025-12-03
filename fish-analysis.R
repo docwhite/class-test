dc20e3251ac068922fb7459060d9815ae93587e2
Ethan White <docwhite@weecology.org>
2025-12-03 14:46:07 +0000

    Support multiple data files

--- fish-analysis.R
+++ fish-analysis.R
@@ -3,7 +3,7 @@ library(readr)
 
 # Analyze the relationship between fish size and scale size
 
-fish_data = read_csv("data/Gaeta_etal_CLC_data.csv")
+fish_data = read_csv("data/Gaeta_etal_CLC_data_1.csv")
 
 fish_data_cat <- fish_data |>
   mutate(length_cat = ifelse(length > 300, "big", "small")) |>
