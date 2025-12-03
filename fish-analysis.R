281785d21e6a281a752d69a9e2e426c7678e7870
Alex-Blochel <alexanderblochel@ufl.edu>
2025-12-03 12:13:15 -0500

    Adjust filter condition for scalelength in analysis

--- fish-analysis.R
+++ fish-analysis.R
@@ -7,4 +7,4 @@ fish_data = read_csv("data/Gaeta_etal_CLC_data_1.csv")
 
 fish_data_cat <- fish_data |>
   mutate(length_cat = ifelse(length > 300, "big", "small")) |>
-  filter(scalelength >=1)
+filter(scalelength >= 0.5)
